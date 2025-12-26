e12958c8c338bbcb777fe1dabdfe60a7eedf122b
Sekh@r Mung@ra <90296564+Sekharmungara@users.noreply.github.com>
2025-12-26 09:12:36 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    public static void main(String[] args) {
+        int rollno = 6613;
+        String sname = "sekhar";
+        String course = "java"; 
+        System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
+        System.out.println("My Name is: " + sname);
+        System.out.println("My Course is: " + course);
+        System.out.println("My Roll Number is: " + rollno);
+        System.out.println("=======================");
+         System.out.println("WELCOME TO JENKINS");
+        
+    }
+}
